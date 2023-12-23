6373dd5e32daf33a15fb437079e0ff50eca61ae5
AyuVirgiana <129573841+AyuVirgiana@users.noreply.github.com>
2023-12-23 20:03:14 +0800

    Add files via upload

---
+++ pages/database.py
@@ -0,0 +1,25 @@
+import streamlit as st 
+import pickle 
+import yaml 
+import pandas as pd 
+cfg = yaml.load(open("config.yaml", "r"), Loader=yaml.FullLoader)
+PKL_PATH = cfg['PATH']["PKL_PATH"]
+
+st.set_page_config(layout="wide")
+
+#Load databse 
+with open(PKL_PATH, 'rb') as file:
+    database = pickle.load(file)
+
+Index, Id, Name, Image  = st.columns([0.5,0.5,3,3])
+
+for idx, person in database.items():
+    with Index:
+        st.write(idx)
+    with Id: 
+        st.write(person['id'])
+    with Name:     
+        st.write(person['name'])
+    with Image:     
+        st.image(person['image'],width=200)
+
